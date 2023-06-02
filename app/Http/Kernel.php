a4b3650ec89b1957352d01a518eba37dc3ee1bbe
@sma.webdeve.backend.14 <eng.abdul.rahman19992@gmail.com>
2023-06-02 16:27:38 +0300

    transaction middleware + response macro service provider

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        ////////////////// Custom ////////////////////
+        'transaction' => \App\Http\Middleware\TransactionMiddleware::class,
     ];
 }
---
+++ app/Http/Middleware/TransactionMiddleware.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Symfony\Component\HttpFoundation\Response;
+use Throwable;
+
+class TransactionMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        DB::beginTransaction();
+        $response = $next($request);
+
+        if (!($response->exception instanceof Throwable)) {
+            DB::commit();
+        } else {
+            DB::rollback();
+        }
+
+        return $response;
+    }
+}
